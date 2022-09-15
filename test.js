a3fb8691d5a1d9c0c807e43c37e057e811c5cf6a
dododong76 <dododong76@gmail.com>
2022-09-15 11:55:50 +0900

    '09151155'

---
+++ test.js
@@ -0,0 +1,13 @@
+el = `<div id='id1' ><div class='class1' data-number='2'>kim</div> </div>`
+// el = el.content
+console.log(el)
+el = el.querySelector('#id1')
+console.log(el)
+
+// console.log(el.textContent)
+console.log(el.innerHTML)
+console.log(el.id)
+console.log(el.class)
+
+
+
